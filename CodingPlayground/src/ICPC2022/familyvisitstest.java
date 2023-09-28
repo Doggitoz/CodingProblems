a123e19c99f0a229eb31c5fde3ec1cba946cf287
Doggitoz <cpwheeler4@gmail.com>
2023-09-28 06:16:37 -0500

    wrong solution to familyvisits

--- CodingPlayground/src/ICPC2022/familyvisitstest.java
+++ CodingPlayground/src/ICPC2022/familyvisitstest.java
@@ -3,7 +3,7 @@ package ICPC2022;
 import java.io.*;
 import java.util.*;
 
-public class familyvisits {
+public class familyvisitstest {
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         int[] parts = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -31,39 +31,43 @@ public class familyvisits {
 
     public static int solve(int low, int high, int[] mess, int[] clean) {
         // Generate total mess over period
-        int[] messTotalByDay = new int[high - low + 2];
-        messTotalByDay[0] = 0;
-        for (int i = 1; i < messTotalByDay.length; i++) {
-            messTotalByDay[i] = messTotalByDay[i - 1] + mess[low - 2 + i];
+        int[] messTotal = new int[high - low + 1];
+        messTotal[0] = mess[low - 1];
+        for (int i = 1; i < messTotal.length; i++) {
+            messTotal[i] = messTotal[i - 1] + mess[low - 1 + i];
         }
 
         // messTotal + 1 so we have a 0 mess row, end - start + 2 so we have a nonclean day
-        short[] curr = new short[messTotalByDay[messTotalByDay.length - 1] + 1];
-        short[] prev = new short[messTotalByDay[messTotalByDay.length - 1] + 1];
+        short[] curr = new short[mess[low - 1] + 1];
+        short[] prev = new short[1001];
 
         for (int i = 1; i < prev.length; i++) {
             prev[i] = -1;
         }
         
         // Work through columns instead of rows
-        for (int i = 1; i < high - low + 2; i++) {
+        for (int i = 0; i < high - low + 1; i++) {
             // j = row/total mess
-            for (int j = 1; j < curr.length; j++) {
-                int cleanDay = clean[low + (i - 2)];
-                int opOne = j < prev.length ? prev[j] : -1;
-                int opTwo = prev[Math.max(0, j - cleanDay)];
-                if (opOne == -1 && opTwo == -1) curr[j] = -1;
-                else if (opOne == -1) curr[j] = (short)(opTwo + 1);
-                else if (opTwo == -1) curr[j] = (short)(opOne);
-                else curr[j] = (short)Math.min(opOne, opTwo + 1); 
+            int cleanDay = clean[low + (i - 1)];
+            for (int j = 0; j < curr.length; j++) {
+                if (cleanDay >= messTotal[i]) curr[j] = 1;
+                int opOne = prev[messTotal[i] + j - messTotal[i - 1]]; // Clean nothing
+                int opTwo = prev[]
+                // int cleanDay = clean[low + (i - 2)];
+                // int opOne = j < prev.length ? prev[j] : -1;
+                // int opTwo = prev[Math.max(0, j - cleanDay)];
+                // if (opOne == -1 && opTwo == -1) curr[j] = -1;
+                // else if (opOne == -1) curr[j] = (short)(opTwo + 1);
+                // else if (opTwo == -1) curr[j] = (short)(opOne);
+                // else curr[j] = (short)Math.min(opOne, opTwo + 1); 
             }
 
             //After work, prev <- curr, clear curr
             if (i == (high - low + 1)) break;
             prev = curr;
-            curr = new short[messTotalByDay[i + 1] + 1];
+            curr = new short[mess[low + i - 1] + 1];
         }
 
-        return curr[curr.length - 1];
+        return curr[mess[high - 1]];
     }
 }
