bb62d1a61e9a8d75452312a552462248dd787285
Coleton Wheeler <cpwheeler4@gmail.com>
2024-02-26 13:48:53 -0600

    sdfrg

---
+++ CodingPlayground/src/ICPC2023/crammingforfinals.java
@@ -0,0 +1,11 @@
+package ICPC2023;
+
+import java.io.*;
+import java.util.*;
+
+public class crammingforfinals {
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
+        
+    }
+}
