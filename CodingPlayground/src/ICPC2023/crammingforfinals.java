c71a14e0750992c6b89387e9aa16592004ed8b13
Doggitoz <cpwheeler4@gmail.com>
2024-02-28 17:17:14 -0600

    Streets Behind Solved

--- CodingPlayground/src/ICPC2023/crammingforfinals.java
+++ CodingPlayground/src/ICPC2023/crammingforfinals.java
@@ -5,7 +5,45 @@ import java.util.*;
 
 public class crammingforfinals {
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer, ArrayList<Interval>> rows = new HashMap<>();
+        int[] rcdn = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int ds = rcdn[2] * rcdn[2];
+
+        int[] offsets = new int[rcdn[2] + 1];
+
+        for (int i = 0; i <= rcdn[2]; i++) {
+            offsets[i] = (int)Math.floor(Math.sqrt(ds - Math.pow(i,2)));
+        }
+
+        for (int s = 0; s < rcdn[3]; s++) {
+            int[] pos = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int i = -rcdn[2]; i <= rcdn[2]; i++) {
+                if (pos[0] + i < 1) continue;
+                if (pos[0] + i > rcdn[0]) continue;
+                Interval interval = new Interval(pos[1] - offsets[Math.abs(i)], 
+                    pos[1] + offsets[Math.abs(i)]);
+                if (interval.start < 1) interval.start = 1;
+                if (interval.end > rcdn[1]) interval.end = rcdn[1];
+                if (rows.containsKey(pos[0] + i)) {
+                    rows.get(pos[0] + i).add(interval);
+                }
+                else {
+                    ArrayList<Interval> a = new ArrayList<>();
+                    a.add(interval);
+                    rows.put(pos[0] + i, a);
+                }
+            }
+        }
+        System.out.println("test");
+    }
+
+    public static class Interval {
+        public int start;
+        public int end;
+        public Interval(int a, int b) {
+            start = a;
+            end = b;
+        }
     }
 }
--- CodingPlayground/src/ICPC2023/streetsbehind.java
+++ CodingPlayground/src/ICPC2023/streetsbehind.java
@@ -7,31 +7,32 @@ public class streetsbehind {
     public static void main(String[] args) throws IOException {
         int t = Integer.parseInt(in.readLine());
         for (int i = 0; i < t; i++) {
-            double[] nkab = Arrays.stream(in.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+            int[] nkab = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int numLoops = 0;
             double n = nkab[0];
             double k = nkab[1];
+            int total = (int) (n + k);
             double a = nkab[2];
             double b = nkab[3];
             double dist = b - a;
             while (k > 0) {
-                int newSerious = (int)Math.floor((b/a) * n - n);
+                int newSerious = (int)Math.floor((b * n) / a - n);
                 if (newSerious == 0) {
                     numLoops = -1;
                     break;
                 }
-                if (newSerious == 1) {
-                    double newN = Math.ceil((2*a)/dist);
-                    double stepsSkipped = newN - n;
-                    k = k - stepsSkipped;
-                    numLoops += stepsSkipped;
-                    n = newN;
+                double newN = Math.ceil(((newSerious + 1)*a)/dist);
+                if (newN > total) {
+                    newN = total;
                 }
-                else {
-                    n = n + newSerious;
-                    k = k - newSerious;
-                    numLoops++;
+                double amtToAdd = newN - n;
+                if (amtToAdd % newSerious != 0) {
+                    amtToAdd += newSerious - (amtToAdd % newSerious);
                 }
+                int steps = (int)(amtToAdd / newSerious);
+                k -= amtToAdd;
+                n += amtToAdd;
+                numLoops += steps;
             }
             System.out.println(numLoops);
         }
